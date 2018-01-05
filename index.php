c4ee63a81a80c595e079f871e69d98a57b72a790
Umar <umark.ws@gmail.com>
2018-01-05 20:13:03 +0000

    Seperated homepage slideshow into includes header-banner.php

--- index.php
+++ index.php
@@ -8,39 +8,7 @@
 <!--Main Content Start-->
 <main class="cd-main-content">
 
-<section id="home" class="section-content">
-<div class="container-fluid">
-    <div class="row">
-			<div id="carousel-main-pictures" class="carousel slide" data-ride="carousel" data-interval="8000" data-pause="false">
-	        <!-- Wrapper for slides -->
-	        <div class="carousel-inner" role="listbox">
-            <!--<div class="item active">
-              <img src="img/slideshow/new-year-slideshow.jpg">
-            </div>-->
-	            <div class="item active">
-	              <img src="img/slideshow/1.jpg">
-	            </div>
-	            <div class="item">
-	              <img src="img/slideshow/2.jpg">
-	            </div>
-							<div class="item">
-	              <img src="img/slideshow/3.jpg">
-	            </div>
-							<div class="item">
-	              <img src="img/slideshow/4.jpg">
-	            </div>
-							<div class="item">
-	              <img src="img/slideshow/5.jpg">
-	            </div>
-	        </div>
-	    </div>
-
-    <div class="col-md-6 col-md-offset-3 text-center ">
-          <h1 style="font-family:nk-font; letter-spacing: .03em; font-style:normal;  text-transform: none; font-size: 40px;">Best Indian Restaurant In London<br /><span style="font-size: 25px;">Winner of Asian Curry Award</span></h1>
-    </div>
-    </div>
-</div>
-</section>
+<?php include ('includes/homepage-banner.php'); ?>
 
         <section id="welcome">
         <div class="container-fluid">
