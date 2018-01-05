10ed02d0f1bc7e03937306afa7159e610c6182fb
Umar <umark.ws@gmail.com>
2018-01-05 20:03:31 +0000

    Seperated header.php seo into includes header-seo.php

---
+++ includes/header-seo.php
@@ -0,0 +1,5 @@
+<meta name="description" content= "Looking for Indian restaurants in Camden Town? Namaaste Kitchen, modern indian restaurant in Central London Camden Town offers healthy Indian cuisine, Grills, Tandoori &amp curries without costing you Micheline starred restaurants price &amp  best catering services in London." />
+
+<meta name="keywords" content="Indian restaurants in london, Restaurants in Camden Town, camden town indian restaurants NW1, indian catering services london, Indian Take away Camden Town" />
+</head>
+<body>
--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php include ('includes/header.php'); ?>
+<?php include ('includes/header-seo.php'); ?>
 
-  <meta name="description" content= "Looking for Indian restaurants in Camden Town? Namaaste Kitchen, modern indian restaurant in Central London Camden Town offers healthy Indian cuisine, Grills, Tandoori &amp curries without costing you Micheline starred restaurants price &amp  best catering services in London." />
-
-  <meta name="keywords" content="Indian restaurants in london, Restaurants in Camden Town, camden town indian restaurants NW1, indian catering services london, Indian Take away Camden Town" />
-</head>
-<body>
 	<header>
 		<?php include ('main-nav.php'); ?>
 	</header>
